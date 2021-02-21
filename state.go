11ce4343c30efeccc0d3dd1d2f4985b3e5d60ebb
gtongy <32930281+gtongy@users.noreply.github.com>
2021-02-21 21:40:06 +0900

    modify thread safe variables interface

--- state.go
+++ state.go
@@ -1,12 +1,14 @@
 package main
 
+import "sync/atomic"
+
 const (
 	Follower RaftState = iota
 	Candidate
 	Leader
 )
 
-type RaftState uint64
+type RaftState uint32
 
 type StableStore interface {
 	Set(key []byte, val []byte) error
@@ -14,4 +16,54 @@ type StableStore interface {
 
 	SetUint64(key []byte, val uint64) error
 	GetUint64(key []byte) (uint64, error)
+}
+
+type raftState struct {
+	state       RaftState
+	currentTerm uint64
+	lastLog     uint64
+	commitIndex uint64
+	lastApplied uint64
+}
+
+func (r *raftState) getState() RaftState {
+	stateAddr := (*uint32)(&r.state)
+	return RaftState(atomic.LoadUint32(stateAddr))
+}
+
+func (r *raftState) setState(s RaftState) {
+	stateAddr := (*uint32)(&r.state)
+	atomic.StoreUint32(stateAddr, uint32(s))
+}
+
+func (r *raftState) getCurrentTerm() uint64 {
+	return atomic.LoadUint64(&r.currentTerm)
+}
+
+func (r *raftState) setCurrentTerm(term uint64) {
+	atomic.StoreUint64(&r.currentTerm, term)
+}
+
+func (r *raftState) getLastLog() uint64 {
+	return atomic.LoadUint64(&r.lastLog)
+}
+
+func (r *raftState) setLastLog(term uint64) {
+	atomic.StoreUint64(&r.lastLog, term)
+}
+
+func (r *raftState) getCommitIndex() uint64 {
+	return atomic.LoadUint64(&r.commitIndex)
+}
+
+func (r *raftState) setCommitIndex(term uint64) {
+	atomic.StoreUint64(&r.commitIndex, term)
+}
+
+func (r *raftState) getLastApplied() uint64 {
+	return atomic.LoadUint64(&r.lastApplied)
+}
+
+func (r *raftState) setLastApplied(term uint64) {
+	atomic.StoreUint64(&r.lastApplied, term)
 }
